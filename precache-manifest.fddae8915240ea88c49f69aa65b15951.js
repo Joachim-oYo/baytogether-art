4dd8d50302df9d197a4e11cd5166cb4b9f6b9539
Jeremy Joachim <7944679+Joachim-oYo@users.noreply.github.com>
2020-05-05 23:53:48 -0700

    Updates

---
+++ precache-manifest.fddae8915240ea88c49f69aa65b15951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953cce67d137e713610db792b186ac58",
+    "url": "/baytogether-art/index.html"
+  },
+  {
+    "revision": "af782b6d78b7941981e2",
+    "url": "/baytogether-art/static/css/main.f0fbdfe7.chunk.css"
+  },
+  {
+    "revision": "d576d94d72a57651439d",
+    "url": "/baytogether-art/static/js/2.7fe721bc.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/baytogether-art/static/js/2.7fe721bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af782b6d78b7941981e2",
+    "url": "/baytogether-art/static/js/main.12d8c7ee.chunk.js"
+  },
+  {
+    "revision": "9817e89a0a842f50d2d6",
+    "url": "/baytogether-art/static/js/runtime-main.e1bcab55.js"
+  }
+]);
