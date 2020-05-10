51e11a412744bac28e7ab19f746ea133f65bb9e8
Jeremy Joachim <7944679+Joachim-oYo@users.noreply.github.com>
2020-05-10 03:48:54 -0700

    got the svg parsing and loading working well

---
+++ tools/svg-parser/src/index.js
@@ -0,0 +1,33 @@
+var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
+
+var results = [];
+var count = 0;
+
+fs.readdir(path.join(__dirname, '/svg'), function(err, files) {
+    files
+        .filter(function(file) { return file.substr(-4) === '.svg'; })
+        .forEach(function(file) { 
+            fs.readFile(path.join(__dirname, '/svg')+'/'+file, 'utf-8', function(err, data) { 
+                if (err) { return console.error(err); }
+                var $ = cheerio.load(data, { xmlMode: true });
+                var data = getDatum($);
+                results.push(data);
+
+                count++;
+                if (files.length === count) { generateJson(); }
+            }); 
+        });
+});
+
+function getDatum($) {
+  return [$('svg').attr('viewBox'), $('path').attr('d')];
+}
+
+function generateJson() {
+  fs.writeFile('./svgPaths.json', JSON.stringify(results), 'utf8', function () {
+    console.log('completed!');
+    process.exit(); 
+  });
+}
